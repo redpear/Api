ce6f9bb652a2012b0bfc83b96137a3d22f8fbd1f
Angie Burk <redpear@mac.com>
2024-11-21 23:39:37 -0800

    Backend working with frontend grid

---
+++ Data/HouseDbContext.cs
@@ -0,0 +1,25 @@
+
+using Microsoft.EntityFrameworkCore;
+
+public class HouseDbContext:DbContext
+{
+    public DbSet<HouseEntity> Houses => Set<HouseEntity>();
+
+    public HouseDbContext(DbContextOptions<HouseDbContext> o):base(o)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+            optionsBuilder
+                .UseSqlite($"Data Source={Path.Join(path, "houses2.db")}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        SeedData.Seed(builder);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<HouseDbContext>(o =>
+    o.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+builder.Services.AddScoped<IHouseRepository, HouseRepository>();
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(p => p.WithOrigins("http://localhost:3000")
+    .AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+
+app.UseHttpsRedirection();
+
+app.MapGet("/houses", (IHouseRepository repo) => repo.GetAll());
+
+app.Run();
+
